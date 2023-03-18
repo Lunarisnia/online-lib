10cd0ba08f48aafaaff0cb9afc209c98a05364ab
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-03-18 19:25:09 +0700

    refactor(user): removed user route and controller since we assume the user already exist

--- src/routers/user.ro.ts
+++
@@ -1,8 +0,0 @@
-import { wrap } from "../services/error/errorHandler";
-import { addUser, getUsers } from "../controllers/user.co";
-import { Router } from "express";
-
-export default (router: Router) => {
-  router.get("/", wrap(getUsers));
-  router.post("/", wrap(addUser));
-};
