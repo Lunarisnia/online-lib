0311605f632225281130d443d9c3686a2a10d382
Rio Arswendo Rachmad <r.arswendo.r@gmail.com>
2023-03-19 13:55:13 +0700

    feat(test): add integration test for auth and admin

---
+++ test/integration/auth.test.ts
@@ -0,0 +1,38 @@
+import request from "supertest";
+import { server } from "../../src/server";
+import { BorrowingScheduleMockDBAdapter } from "../../src/database/drivers/mockDatabase/borrowingSchedule.model";
+beforeEach(() => {
+  jest.resetAllMocks();
+});
+
+describe("GET /v1/admin/borrow/list", () => {
+  it("Responds with an error because the token is invalid", async () => {
+    const response = await request(server())
+      .get("/v1/admin/borrow/list")
+      .set("Authorization", `Bearer `);
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.error.id).toEqual(1008);
+    expect(response.body.error.name).toEqual("JsonWebTokenError");
+  });
+});
+
+describe("GET /v1/borrow", () => {
+  it("Responds with an error because the token is invalid", async () => {
+    const response = await request(server())
+      .get("/v1/borrow")
+      .set("Authorization", `Bearer `);
+
+    expect(response.statusCode).toEqual(401);
+    expect(response.body.error.id).toEqual(1008);
+    expect(response.body.error.name).toEqual("JsonWebTokenError");
+  });
+});
+
+describe("GET /", () => {
+  it("Responds with a welcome message", async () => {
+    const response = await request(server()).get("/");
+
+    expect(response.body.data).toEqual("OK");
+  });
+});
